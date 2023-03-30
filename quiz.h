72a92320be5888ebb1744d3e01249575c88dce19
nolanmargot <84375545+nolanmargot@users.noreply.github.com>
2023-03-30 14:14:28 +0200

    Add files via upload

---
+++ quiz.h
@@ -0,0 +1,15 @@
+#ifndef QUIZ_H
+#define QUIZ_H
+
+#include <iostream>
+#include <cmath>
+#include <stdexcept>
+#include <fstream>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <map>
+#include <cstdint>
+
+#endif
